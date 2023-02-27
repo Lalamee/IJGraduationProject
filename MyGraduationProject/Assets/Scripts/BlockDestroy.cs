233aa9f897c2907b3163a7d62c67af516213e316
RedWave <kurilov6dv@gmail.com>
2023-02-27 18:30:39 +0300

    0.1
    Make base for destroy and player

---
+++ MyGraduationProject/Assets/Scripts/BlockDestroy.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BlockDestroy : MonoBehaviour
+{
+     private Tilemap _cave;
+    private GridLayout _grid;
+    private Vector2 _mousePosition; 
+    private Vector3Int _blockPosition;
+
+    private void Start()
+    {
+        _cave = GetComponent<Tilemap>();
+        _grid = GetComponent<GridLayout>();
+    }
+
+    private void Update()
+    {
+        _mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        _blockPosition = _grid.WorldToCell(_mousePosition); 
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            _cave.SetTile(_blockPosition, null);
+        }
+    }
+}
