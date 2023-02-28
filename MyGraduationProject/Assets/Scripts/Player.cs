a60b2428e0d80e76fed93a25b3617e7a6cb640c3
RedWave <kurilov6dv@gmail.com>
2023-02-28 23:20:30 +0300

    1.1.1

--- MyGraduationProject/Assets/Scripts/Player.cs
+++ MyGraduationProject/Assets/Scripts/Player.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float _speed;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.Translate(_speed * Time.deltaTime,0,0 );
     }
 }
