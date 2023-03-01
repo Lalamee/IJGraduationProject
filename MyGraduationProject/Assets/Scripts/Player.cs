b962206758dd606b24388ef256aa2879c1206f18
RedWave <kurilov6dv@gmail.com>
2023-03-01 15:42:21 +0300

    (

--- MyGraduationProject/Assets/Scripts/Player.cs
+++ MyGraduationProject/Assets/Scripts/Player.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _range;
+    [SerializeField] private GameObject _blocks;
 
     private void Update()
     {
         transform.Translate(_speed * Time.deltaTime,0,0 );
+
+        CheckDistance(_range, _blocks);
+    }
+
+    private void CheckDistance(float range, GameObject block)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, range);
+
+        if (hit.collider == block)
+            transform.Translate(_speed * Time.deltaTime,0,0 );
     }
 }
