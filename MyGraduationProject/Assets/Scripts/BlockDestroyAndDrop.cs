d2698f2e2f16770f54d815c35d250264a2f93df1
RedWave <kurilov6dv@gmail.com>
2023-03-09 16:54:02 +0300

    2

---
+++ MyGraduationProject/Assets/Scripts/BlockDestroyAndDrop.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BlockDestroyAndDrop : MonoBehaviour
+{
+    [SerializeField] private GameObject _drop;
+    
+    private Tilemap _cave;
+    private GridLayout _grid;
+    private Vector2 _mousePosition; 
+    private Vector3Int _blockPosition;
+
+    private void Start()
+    {
+        _cave = GetComponent<Tilemap>();
+        _grid = GetComponent<GridLayout>();
+    }
+
+    private void Update()
+    {
+        Vector3Int blockPosition = FindBlockPosition();
+        bool isDestroy = IsDestroy(blockPosition);
+        
+    }
+
+    private Vector3Int FindBlockPosition()
+    {
+        _mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        _blockPosition = _grid.WorldToCell(_mousePosition);
+
+        return _blockPosition;
+    }
+
+    private bool IsDestroy(Vector3Int blockPosition)
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _cave.SetTile(blockPosition, null);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
