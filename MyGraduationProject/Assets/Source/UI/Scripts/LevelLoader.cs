f30a80be9a69a4532203ff4930c7c6d6c544300d
Dmitry Kurilov <kurilov6dv@gmail.com>
2023-07-21 18:33:24 +0300

    1

--- MyGraduationProject/Assets/Source/UI/Scripts/LevelLoader.cs
+++ MyGraduationProject/Assets/Source/UI/Scripts/LevelLoader.cs
@@ -1,10 +1,17 @@
+using IJunior.TypedScenes;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelLoader : MonoBehaviour
+public class LevelLoader : MonoBehaviour, ISceneLoadHandler<int>
 {
     public void LoadLevel(int levelNumber)
     {
         SceneManager.LoadScene(levelNumber);
+        
+    }
+
+    public void OnSceneLoaded(int argument)
+    {
+       
     }
 }
